c30af013f9af1de013887582cbb72f32f7adf26c
rajavenkatesh04 <rg3941@srmist.edu.in>
2025-02-20 01:47:59 +0530

    Added analytics tracker

--- app/page.js
+++ app/page.js
@@ -2,11 +2,12 @@ import Image from "next/image";
 import Navbar from "@/components/Navbar";
 import Header from "@/components/Header";
 import About from "@/components/About";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   return (
     <>
-      <Navbar /><Header /><About />
+      <Navbar /><Header /><About /><Analytics
     </>
   );
 }
