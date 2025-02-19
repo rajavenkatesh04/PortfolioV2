ed80fed1dbb5e964133de60bfaa9385c7dd5d373
rajavenkatesh04 <rg3941@srmist.edu.in>
2025-02-20 02:47:22 +0530

    Add about me section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,6 +8,7 @@ export default {
   theme: {
     extend: {
       colors: {
+
         lightHover: '#fcf4ff',
         darkHover: '#2a004a',
         darkTheme: '#11001f',
@@ -18,6 +19,11 @@ export default {
         Ovo: ['Ovo', 'sans-serif'],
 
       },
+
+      boxShadow: {
+        black: '4px 4px 0 #000',
+        white: '4px 4px 0 #fff',
+      }
     },
   },
   plugins: [],
