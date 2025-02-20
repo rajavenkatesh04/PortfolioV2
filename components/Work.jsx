c7bbede35b243b3e86ec20152d362cda7ea54e6a
rajavenkatesh04 <rg3941@srmist.edu.in>
2025-02-20 21:32:28 +0530

    Add work section

--- components/Work.jsx
+++ components/Work.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import {assets, workData} from "@/assets/assets";
+import Image from "next/image";
 
 export default function Work() {
     return (
@@ -6,6 +8,26 @@ export default function Work() {
             <h4 className={"text-center mb-2 text-lg font-Ovo"}>My Portfolio</h4>
             <h2 className={"text-center text-5xl font-Ovo"}>My Latest work</h2>
             <p className={"text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo"}>I'm a Java developer from India, experienced in WebDev & Java I aim to create stunning minimalistic websites.</p>
+
+            <div className={"grid grid-cols-auto my-10 gap-5 font-Ovo"}>
+                {workData.map((project, index) => (
+                    <div key={index} className={"aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group"} style={{backgroundImage: `url(${project.bgImage})`}}>
+
+                        <div className={"bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7"}>
+                            <div>
+                                <h2 className={"font-semibold"}>{project.title}</h2>
+                                <p className={"text-sm text-gray-700"}>{project.description}</p>
+                            </div>
+                            <div className={"border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition"}>
+                                <Image src={assets.send_icon} alt={"send_icon"} className={"w-5"} />
+                            </div>
+                        </div>
+
+                    </div>
+                ))}
+            </div>
+
+            <a href={""} className={"w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500"}>Show more <Image src={assets.right_arrow_bold} alt={'right arrow'} className={"w-4"} /></a>
         </div>
     )
 }
