64901e5131b29a8774c9037fcc96218a9a529bc0
rajavenkatesh04 <rg3941@srmist.edu.in>
2025-02-20 21:02:48 +0530

    Add Services section

--- app/page.js
+++ app/page.js
@@ -3,11 +3,13 @@ import Navbar from "@/components/Navbar";
 import Header from "@/components/Header";
 import About from "@/components/About";
 import { Analytics } from "@vercel/analytics/react"
+import Services from "@/components/Services";
+import Work from "@/components/Work";
 
 export default function Home() {
   return (
     <>
-      <Navbar /><Header /><About /><Analytics />
+      <Navbar /><Header /><About /><Services /><Work /><Analytics />
     </>
   );
 }
---
+++ components/Work.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Work() {
+    return (
+        <div id={"work"} className={"w-full px-[12%] py-10 scroll-mt-20"}>
+            <h4 className={"text-center mb-2 text-lg font-Ovo"}>My Portfolio</h4>
+            <h2 className={"text-center text-5xl font-Ovo"}>My Latest work</h2>
+            <p className={"text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo"}>I'm a Java developer from India, experienced in WebDev & Java I aim to create stunning minimalistic websites.</p>
+        </div>
+    )
+}
